6cfade49b4df1b6855abbb230f9755d3910ca632
amiraabouhadid <amiraabouhadid@gmail.com>
2023-02-06 16:32:49 +0200

    rewrite mnemonics placeholder

--- examples/direct/main.ts
+++ examples/direct/main.ts
@@ -5,7 +5,7 @@ import { createDirectClient, sendDirectRequest } from "../../lib/direct";
 async function main() {
 
     // create client 
-    const client = await createDirectClient(`ws://localhost:8080/`, "test_client", "drama govern gossip audit mixed silent voice mule wonder protect latin idea", 'sr25519');
+    const client = await createDirectClient(`ws://localhost:8080/`, "test_client", "<mnemonics_here>", 'sr25519');
     // connect socket
     const socket = connect(client);
     // send request
