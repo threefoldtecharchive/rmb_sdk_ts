f860bda628e0efad4f18d33a37367dbce5dbf5a6
amiraabouhadid <amiraabouhadid@gmail.com>
2023-02-06 17:06:23 +0200

    add response return string from listening function

--- examples/direct/main.ts
+++ examples/direct/main.ts
@@ -5,13 +5,15 @@ import { createDirectClient, sendDirectRequest } from "../../lib/direct";
 async function main() {
 
     // create client 
-    const client = await createDirectClient(`ws://localhost:8080/`, "test_client", "<mnemonics_here>", 'sr25519');
+    const client = await createDirectClient(`ws://localhost:8080/`, "test_client", "drama govern gossip audit mixed silent voice mule wonder protect latin idea", 'sr25519');
     // connect socket
     const socket = connect(client);
     // send request
     const requestID = sendDirectRequest(client, socket, "calculator.add", [10.6, 20], 1292);
-    // listen
-    listening(requestID, client, socket);
+    // get response
+    const response = await listening(requestID, client, socket);
+    // print response
+    console.log(response);
 }
 
 
--- lib/connect.ts
+++ lib/connect.ts
@@ -15,28 +15,43 @@ export function connect(client: directClientInterface) {
     return ws;
 }
 export function listening(requestID: string, client: directClientInterface, socket: ReconnectingWebSocket) {
-    socket.onmessage = (e) => {
-        console.log("waiting response...");
-        // need to verify first
-        if (client.responses.get(requestID)) {
-            const receivedEnvelope = Envelope.deserializeBinary(e.data);
+    return new Promise((resolve, reject) => {
 
-            const response = receivedEnvelope.getResponse();
+        socket.onmessage = (e) => {
 
-            if (response) {
-                const reply = response.getReply();
-                const err = response.getError();
-                if (reply) {
-                    const dataReceieved = reply.getData();
-                    const decodedData = new TextDecoder('utf8').decode(Buffer.from(dataReceieved))
-                    console.log(`response: ${JSON.parse(decodedData)}`);
-                } else if (err) {
-                    console.log(`error: ${err.getCode()} ${err.getMessage()}`);
+            console.log("waiting response...");
+            // need to verify first
+            if (client.responses.get(requestID)) {
+                const receivedEnvelope = Envelope.deserializeBinary(e.data);
+
+                const response = receivedEnvelope.getResponse();
+
+                if (response) {
+                    const reply = response.getReply();
+                    const err = response.getError();
+                    if (reply) {
+                        const dataReceieved = reply.getData();
+                        const decodedData = new TextDecoder('utf8').decode(Buffer.from(dataReceieved))
+                        const responseString = `response: ${JSON.parse(decodedData)}`;
+                        resolve(responseString);
+                    } else if (err) {
+                        const errString = `error: ${err.getCode()} ${err.getMessage()}`
+                        resolve(errString)
+
+                    }
                 }
+                // remove no longer needed key value pair
+                client.responses.delete(requestID);
             }
-            // remove no longer needed key value pair
-            client.responses.delete(requestID);
+
+
         }
 
-    }
+
+
+
+    })
+
+
+
 }
