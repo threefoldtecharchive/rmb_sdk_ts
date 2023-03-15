a58a1b340ab079850dae516e8aa6314ee5264716
amiraabouhadid <amiraabouhadid@gmail.com>
2023-03-15 11:48:09 +0200

    merge development

--- examples/direct/node.ts
+++ examples/direct/node.ts
@@ -3,14 +3,14 @@ import { Client } from "../../lib/client";
 
 async function main() {
     // create client 
-    const client = new Client("wss://tfchain.dev.grid.tf/ws", `wss://relay.dev.grid.tf/`, "<mnemonics>", "test_client", 'sr25519', 5);
+    const client = new Client("wss://tfchain.dev.grid.tf/ws", `wss://relay.dev.grid.tf/`, "route visual hundred rabbit wet crunch ice castle milk model inherit outside", "test_client", 'sr25519', 5);
 
     try {
 
         // connect socket
         await client.connect()
         // send request
-        const requestID = await client.send("zos.deployment.get", JSON.stringify({ "contract_id": 19038 }), 22, 5);
+        const requestID = await client.send("zos.deployment.get", JSON.stringify({ "contract_id": 19461 }), 25, 5);
         // get response
 
         const response = await client.read(requestID);
